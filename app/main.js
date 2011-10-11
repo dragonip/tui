57bacd3adeb59a5e9f19674b3586c12c246abb3d
Peter StJ <regardingscot@gmail.com>
2011-10-11 15:46:43 +0300

    MAke sthe welcome screen pop on load

--- app/main.js
+++ app/main.js
@@ -1,49 +1,48 @@
 //(function() {
-	require.config({
-		baseUrl: "app",
-		paths: {
-			"types" : "../library/js/types",
-			"window": "../library/js/window",
-			"utils/autoid": "../library/js/utils/autoid",
-			"templates/compiler" :"../library/js/templates/compiler",
-			"support": "../library/js/support",
-			"shims" : "../library/js/shims",
-			"oop" : "../library/js/oop",
-			"nls" : "../library/js/nls",
-			"net" : "../library/js/net",
-			"loader" : "../library/js/loader",
-			"host" : "../library/js/nost",
-			"env/exports" : "../library/js/env/exports",
-			"dom" : "../library/js/dom",
-			"debug" : "../library/js/debug",
-			"array" : "../library/js/array",
-			"text" : "../library/js/text",
-		},
-		urlArgs: "bust=" +  (new Date()).getTime()
-	});
-	var options, initmodules,
-		assets = {
-			initCSS: ['app/css/reset.css', 'app/css/appselector.css'],
-			initModules: ['ui/appselector', 'utils/events', 'dmc/dmc'],
-			clearActions: {}
-		},
-		options = {
-			debug: true,
-			nodejs: false,
-			version: '0.1'
-		}
-	
+require.config({
+	baseUrl: "app",
+	paths: {
+		"types": "../library/js/types",
+		"window": "../library/js/window",
+		"utils/autoid": "../library/js/utils/autoid",
+		"templates/compiler": "../library/js/templates/compiler",
+		"support": "../library/js/support",
+		"shims": "../library/js/shims",
+		"oop": "../library/js/oop",
+		"nls": "../library/js/nls",
+		"net": "../library/js/net",
+		"loader": "../library/js/loader",
+		"host": "../library/js/nost",
+		"env/exports": "../library/js/env/exports",
+		"dom": "../library/js/dom",
+		"debug": "../library/js/debug",
+		"array": "../library/js/array",
+		"text": "../library/js/text",
+	},
+	urlArgs: "bust=" + (new Date()).getTime()
+});
+var options, initmodules, assets = {
+	initCSS: ['app/css/reset.css', 'app/css/appselector.css'],
+	initModules: ['ui/appselector', 'utils/events', 'dmc/dmc'],
+	clearActions: {}
+},
+	options = {
+		debug: true,
+		nodejs: false,
+		version: '0.1'
+	}
+
 	window.tui = {
 		loadIndicator: null,
 		options: options,
 		currentActiveApp: null,
 		appRequested: false,
-		appModuleAdded: function(app) {
+		appModuleAdded: function (app) {
 			this.logger.log(['Setting current active app to', app]);
 			this.currentActiveApp = app;
 			app.Start();
 		},
-		setContainerVisibility: function(bool) {
+		setContainerVisibility: function (bool) {
 			if (bool) {
 				this.mainContainer.style.opacity = 0.2;
 			} else {
@@ -51,7 +50,7 @@
 			}
 		},
 		apps: {
-			signals: function(signaltype, opts) {
+			signals: function (signaltype, opts) {
 				switch (signaltype) {
 				case 'ready':
 					tui.logger.log(['TUI received signal ready for app', opts]);
@@ -65,7 +64,7 @@
 				}
 			}
 		},
-		loadApp: function(appobj) {
+		loadApp: function (appobj) {
 			this.loadIndicator.show('Loading ' + appobj.name + '...');
 			if (this.currentActiveApp !== null) {
 				if (typeof this.currentActiveApp.Pause === 'function') {
@@ -74,58 +73,67 @@
 					this.currentActiveApp.Stop();
 				}
 			}
-			require([appobj.module], function(appmodule) {
+			require([appobj.module], function (appmodule) {
 				tui.appModuleAdded(appmodule);
 			});
 		}
 	};
-	//Load the real TUI
-	function loadTUI() {
-		require(['ui/appselector', 'dmc/dmc'], function(Mappsel, Mdmc) {
-			require([(Mdmc.isNative())?'app/paths/stb.js':'app/paths/browser.js','data/applist'], function(paths, apps) {
-				tui.options.paths = paths;
-				tui.loadIndicator.hide();
-			});
-		});
-	}
-	if (tui.options.debug) {
-		window.DEBUG = {
-			popup: false 
-		};
-	}
-	require(['ui/throbber'], function(t) {
-		var a = document.createElement('div');
-		a.setAttribute('id', 'maincontainer');
-		a.style.height = window.innerHeight + 'px';
-		a.style.width = window.innerWidth + 'px';
-		document.body.setAttribute('style', 'width: '+ window.innerWidth + 'px; height: ' + window.innerHeight + 'px;');
-		tui.mainContainer = a;
-		document.querySelector('body').appendChild(a);
-		tui.loadIndicator = {
-			show: function(text) {
-				t.start({
-					element: document.body,
-					text: text || 'Loading TUI...'
-				});
-			},
-			hide: function() {
-				//tui.logger.log(t);
-				t.stop();
-			}
-		};
-		tui.loadIndicator.show();
+//Load the real TUI
 
-		//Request our logger utility and then the static loader
-		require(['debug/console'], function(logger) {
-			//
-			// just keep reference to the logger in case, otherwise use 'window.pcli browser wide
-			// 
-			tui.logger = logger.getInstance('main');
-			tui.logger.log('Load the intrinsic loader now..');
-			require(['loader/loader'], function(loader) {
-				tui.logger.log(['Loaded intrinsic loader', 'Should switch to jquery and net/xrh to be browser independent']);
-				loader.loadCSS(['app/css/reset.css', 'app/css/appselector.css'], loadTUI);
+
+function loadTUI() {
+	require(['ui/appselector', 'dmc/dmc'], function (Mappsel, Mdmc) {
+		require([(Mdmc.isNative()) ? 'app/paths/stb.js' : 'app/paths/browser.js', 'data/applist'], function (paths, apps) {
+			tui.options.paths = paths;
+			tui.loadIndicator.hide();
+			tui.loadApp({
+				name: 'Start',
+				apptag: 'start',
+				module: 'apps/start',
+				icon: 'imgs/start_screen_icon.png'
 			});
 		});
 	});
+}
+if (tui.options.debug) {
+	window.DEBUG = {
+		popup: false
+	};
+}
+require(['ui/throbber'], function (t) {
+	var a = document.createElement('div');
+	a.setAttribute('id', 'maincontainer');
+	a.style.height = window.innerHeight + 'px';
+	a.style.width = window.innerWidth + 'px';
+	document.body.setAttribute('style', 'width: ' + window.innerWidth + 'px; height: ' + window.innerHeight + 'px;');
+	tui.mainContainer = a;
+	document.querySelector('body').appendChild(a);
+	tui.loadIndicator = {
+		show: function (text) {
+			t.start({
+				element: document.body,
+				text: text || 'Loading TUI...'
+			});
+		},
+		hide: function () {
+			//tui.logger.log(t);
+			t.stop();
+		}
+	};
+	tui.loadIndicator.show();
+
+	//Request our logger utility and then the static loader
+	require(['debug/console'], function (logger) {
+		//
+		// just keep reference to the logger in case, otherwise use 'window.pcli browser wide
+		// 
+		tui.logger = logger.getInstance('main');
+		tui.logger.log('Load the intrinsic loader now..');
+		require(['loader/loader'], function (loader) {
+			tui.logger.log(['Loaded intrinsic loader', 'Should switch to jquery and net/xrh to be browser independent']);
+			loader.loadCSS(['app/css/reset.css', 'app/css/appselector.css'], loadTUI);
+		});
+	});
+});
 //}());
+
