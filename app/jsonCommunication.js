84d4385248431f49ab3284ae5b218825ab7c9a1f
Peter StJ <regardingscot@gmail.com>
2011-10-27 18:26:03 +0300

    Proposal format for new communication mechanics

---
+++ app/jsonCommunication.js
@@ -0,0 +1,17 @@
+{
+	header: {
+		globalCallback: "window.GlobalFunction",
+		sid: "unique ID",
+		run: "danocho's run",
+		transport: "javascript|socket",
+		type: "request|event"
+	},
+	request: {
+		templateurl: "",
+		template: "",
+		data: {}
+	},
+	response: {},
+	status: {}
+}
+
