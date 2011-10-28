f69106bf4ae04f6f8481cff66c285c2935b70815
Peter StJ <regardingscot@gmail.com>
2011-10-28 17:50:56 +0300

    Added new tests

--- app/test.js
+++ app/test.js
@@ -32,17 +32,21 @@ require(['transport/response'], function(response) {
 		console.log("received message from server");
 		response.recall(JSONString);
 	};
-	require(['net/socket', 'transport/request'], function(socket, request) {
-		var myRequest = request.create('getiptvlist');
-		response.register(myRequest, function(data) {
-			alert(data);
-		}, null);
-		myRequest.json.response = {
-			status: 'ok',
-			data: null
-		};
-		myRequest.send();
-		myRequest.disposeInternal();
+	require(['view/mosaictemplate'], function(mt) {
+		
+
 	});
+//	require(['net/socket', 'transport/request'], function(socket, request) {
+//		var myRequest = request.create('getiptvlist');
+//		response.register(myRequest, function(data) {
+//			alert(data);
+//		}, null);
+//		myRequest.json.response = {
+//			status: 'ok',
+//			data: null
+//		};
+//		myRequest.send();
+//		myRequest.disposeInternal();
+//	});
 });
 
