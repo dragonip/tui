42ebe680cb80053e1464faa7a387f76badcf3ac6
Peter StJ <regardingscot@gmail.com>
2011-11-16 15:27:42 +0200

    Fixes for request, added documentation

--- app/transport/request.js
+++ app/transport/request.js
@@ -1,39 +1,52 @@
 define(['transport/requestdata', 'json/json', 'net/socket', 'dmc/dmc'], function(request, json, socket, dmc) {
-	var waiter = {};
-	var JSONRequest = function(run, data, url) {
-		this.json = request.create(run, data, url);
+	/**
+	* Wrapper for JSON requests, it only provides the socket wrapper if needed, used only in browsers, will be stripped off when building
+	* @param {!String} run The method to set in the header
+	* @param {Object} data The data to pass as request body, it will always be set as the object content of object 'request'
+	*/
+	var JSONRequest = function(run, data) {
+		this.json = request.create(run, data);
 	};
+	/**
+	* Returns the serialized data of the request object
+	* @return {JSONString}
+	*/
 	JSONRequest.prototype.getRequestString = function() {
 		return json.serialize(this.json);
 	};
+	/**
+	 * Contains the socket reference if one can be created
+	 * @static
+	 */
 	JSONRequest.prototype.socket = (function() {
 		if (dmc.isNative()) return null;
-		return socket.create('ws://192.168.2.58:7681', 'stb-json-protocol', window.transportReceiver);
+		return socket.create('ws://192.168.2.64:7681', 'stb-json-protocol', window.transportReceiver);
 	})();
-	
-//	JSONRequest.prototype.send = dmc.isNative ? (function(socket) {
-//		return function(callback) {
-//			socket.send(this.getRequestString());
-//		};
-//	})(socket.create('ws://192.168.2.58:7681', 'stb-json-protocol', window.transportReceiver)) : function() {
-//		window.SMJS.send(this.getRequestString());
-//	};
-	JSONRequest.prototype.send = function(callback) {
-		console.log('Sending the request via socket')
+	/**
+	 * Sends the request via the socket, first serializing it
+	 */
+	JSONRequest.prototype.send = function() {
+		console.log('Sending the request via socket',this.getRequestString());
 		if (this.socket !== null) {
-			console.log('Socket exists, send now',this.getRequestString() );
 			this.socket.send(this.getRequestString());
+		} else {
+			console.log('Socket does not exists?');
 		}
 	};
-	
+	/**
+	 * Disposes the data it uses (calling the actual request dispose methods
+	 */
 	JSONRequest.prototype.disposeInternal = function() {
 		this.json.disposeInternal();
 		this.json = null;
 		delete this.json;
 	};
+	/**
+	 * Wraps the object creation functionality
+	 */
 	return {
 		create: function(run, data, url) {
-			return new JSONRequest(run, data, url);
+			return new JSONRequest(run, data);
 		}
 	};
 });
