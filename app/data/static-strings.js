3d8b08d2b93a0f44e79d74de69fb435957b675a2
Peter StJ <regardingscot@gmail.com>
2011-11-21 16:15:30 +0200

    Add translation support

--- app/data/static-strings.js
+++ app/data/static-strings.js
@@ -6,6 +6,98 @@ define({
 		bookmark: "Bookmark",
 		unbookmark: "Unbookmark"
 	},
+	screens: {
+		callcenter: {
+			panels: {
+				bottom: {
+					
+				}
+			},
+			list: {
+				
+			}
+		},
+		sms: {
+			panels: {
+				bottom: {
+					arrows: "Navigate"
+				}
+			},
+			list: {
+				header: {
+					
+				},
+				body: {
+					to: "To:",
+					message: "Text:"
+				}
+			}
+		},
+		phonebook: {
+			panels: {
+				bottom: {
+					ok: "Call now",
+					info: "Contact options"
+				}
+			},
+			list: {
+				header: {
+					name: "Name",
+					number: "Number",
+					speedDial: "SD"
+				}
+			}
+		},
+		voicemail: {
+			panels: {
+				
+			},
+			list: {
+				header: {
+					
+				},
+				body: {
+					caller: "Call from",
+					time: "Time of call"
+				}
+			}
+		},
+		callhistory: {
+			panels: {
+				bottom: {
+					ok: "Call"
+				}
+			},
+			list: {
+				header: {
+					caller: "Caller Name",
+					time: "Time of call",
+					duration: "Call duration",
+					type: "Type"
+				}
+			}
+		},
+		chooser: {
+			panels: {
+				bottom: {
+					arrows: "Select task",
+					ok: "Go to task"
+				}
+			},
+			list: {
+				header: {
+					
+				},
+				body: {
+					callcenter: "Call Centre",
+					history: "Call History",
+					voicemail: "Voice Mail",
+					sms: "Send SMS",
+					phonebook: "Phonebook"			
+				}
+			}
+		}
+	},
 	components: {
 		dialogs: {
 			select: "Select action",
