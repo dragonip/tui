c82d579a9870976d10bc6587e164931aede35d9d
Kaloyan Kolev <kaloqnkolev555@gmail.com>
2011-11-22 13:05:44 +0200

    New dialler New SMS css new icon

--- app/tpl/telephony_callcenter.js
+++ app/tpl/telephony_callcenter.js
@@ -4,97 +4,94 @@ var buf = [];
 with (locals || {}) {
 var interp;
 buf.push('<div');
-buf.push(attrs({ "class": ('tui-multiscreen') + ' ' + ('callCenter') }));
+buf.push(attrs({ "class": ('tui-multiscreen') + ' ' + ('phone-keyboard') }));
 buf.push('><input');
 buf.push(attrs({ 'type':("tel"), 'pattern':("[0-9]"), 'name':("phoneNumber"), "class": ('phoneDisplay') }));
 buf.push('/><div');
-buf.push(attrs({ "class": ('phoneKeyboard') }));
-buf.push('><div');
 buf.push(attrs({ "class": ('row') }));
 buf.push('><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "1"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "2"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "3"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') + ' ' + ('up') }));
 buf.push('><img');
-buf.push(attrs({ 'src':("app/imgs/greenPhone.png"), "class": ('phoneImg') }));
+buf.push(attrs({ 'src':("app/imgs/phone.svg"), "class": ('phone-image') }));
 buf.push('/></div></div><div');
 buf.push(attrs({ "class": ('row') }));
 buf.push('><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "4"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "5"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "6"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') + ' ' + ('down') }));
 buf.push('><img');
-buf.push(attrs({ 'src':("app/imgs/redPhone.png"), "class": ('phoneImg') }));
+buf.push(attrs({ 'src':("app/imgs/phone.svg"), "class": ('phone-image') }));
 buf.push('/></div></div><div');
 buf.push(attrs({ "class": ('row') }));
 buf.push('><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "7"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "8"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "9"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') + ' ' + ('addToPhone') }));
-buf.push('>');
-var __val__ = "Call<br>History"
-buf.push(null == __val__ ? "" : __val__);
-buf.push('</div></div><div');
+buf.push(attrs({ "class": ('phone-btn') + ' ' + ('calls') }));
+buf.push('><img');
+buf.push(attrs({ 'src':("app/imgs/calls.svg"), "class": ('phone-image') }));
+buf.push('/></div></div><div');
 buf.push(attrs({ "class": ('row') }));
 buf.push('><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "*"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "0"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') }));
 buf.push('>');
 var __val__ = "#"
 buf.push(null == __val__ ? "" : __val__);
 buf.push('</div><div');
-buf.push(attrs({ "class": ('phoneBtn') }));
+buf.push(attrs({ "class": ('phone-btn') + ' ' + ('phoneBook') }));
 buf.push('><img');
-buf.push(attrs({ 'src':("app/imgs/phoneBook.png"), "class": ('phoneDictionary') }));
-buf.push('/></div></div></div></div>');
+buf.push(attrs({ 'src':("app/imgs/phone-book.svg"), "class": ('phone-image') }));
+buf.push('/></div></div></div>');
 }
 return buf.join("");
 }return { render: anonymous }; });
