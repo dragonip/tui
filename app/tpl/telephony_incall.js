a863d86ee03ff27a23d883197c78fc7ca87a2eaa
Kaloyan Kolev <kaloqnkolev555@gmail.com>
2011-11-25 13:39:02 +0200

    Adds youtube rating stars Adds in call screen Fixes

--- app/tpl/telephony_incall.js
+++ app/tpl/telephony_incall.js
@@ -4,15 +4,95 @@ var buf = [];
 with (locals || {}) {
 var interp;
 buf.push('<div');
-buf.push(attrs({ "class": ('tui-multiscreen') + ' ' + ('inCallMessage') }));
+buf.push(attrs({ "class": ('call-container') }));
 buf.push('><div');
-buf.push(attrs({ "class": ('callMessage') }));
+buf.push(attrs({ "class": ('scroll-call') }));
+buf.push('><div');
+buf.push(attrs({ "class": ('call-status') }));
 buf.push('><img');
-buf.push(attrs({ 'src':("app/imgs/in_call_icon.png") }));
+buf.push(attrs({ 'src':("app/imgs/avatar.svg") }));
 buf.push('/><div>');
 var __val__ = "Call is in session.."
 buf.push(null == __val__ ? "" : __val__);
-buf.push('</div></div></div>');
+buf.push('<div');
+buf.push(attrs({ "class": ('caller-name') }));
+buf.push('>');
+var __val__ = "John Smith"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div><div');
+buf.push(attrs({ "class": ('caller-info') }));
+buf.push('>');
+var __val__ = "1-222-222-222"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div></div></div><div');
+buf.push(attrs({ "class": ('call-status') }));
+buf.push('><img');
+buf.push(attrs({ 'src':("app/imgs/pause.svg") }));
+buf.push('/><div>');
+var __val__ = "Call is on hold.."
+buf.push(null == __val__ ? "" : __val__);
+buf.push('<div');
+buf.push(attrs({ "class": ('caller-name') }));
+buf.push('>');
+var __val__ = "John Smith"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div><div');
+buf.push(attrs({ "class": ('caller-info') }));
+buf.push('>');
+var __val__ = "1-222-222-222"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div></div></div><div');
+buf.push(attrs({ "class": ('call-status') }));
+buf.push('><img');
+buf.push(attrs({ 'src':("app/imgs/phone-ring.svg") }));
+buf.push('/><div>');
+var __val__ = "Incoming Call.."
+buf.push(null == __val__ ? "" : __val__);
+buf.push('<div');
+buf.push(attrs({ "class": ('caller-name') }));
+buf.push('>');
+var __val__ = "John Smith"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div><div');
+buf.push(attrs({ "class": ('caller-info') }));
+buf.push('>');
+var __val__ = "1-222-222-222"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div></div></div><div');
+buf.push(attrs({ "class": ('call-status') }));
+buf.push('><img');
+buf.push(attrs({ 'src':("app/imgs/dialing.svg") }));
+buf.push('/><div>');
+var __val__ = "Call is in dialing.."
+buf.push(null == __val__ ? "" : __val__);
+buf.push('<div');
+buf.push(attrs({ "class": ('caller-name') }));
+buf.push('>');
+var __val__ = "John Smith"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div><div');
+buf.push(attrs({ "class": ('caller-info') }));
+buf.push('>');
+var __val__ = "1-222-222-222"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div></div></div><div');
+buf.push(attrs({ "class": ('call-status') }));
+buf.push('><img');
+buf.push(attrs({ 'src':("app/imgs/conferance.svg") }));
+buf.push('/><div>');
+var __val__ = "Call is in conferance.."
+buf.push(null == __val__ ? "" : __val__);
+buf.push('<div');
+buf.push(attrs({ "class": ('caller-name') }));
+buf.push('>');
+var __val__ = "John Smith"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div><div');
+buf.push(attrs({ "class": ('caller-info') }));
+buf.push('>');
+var __val__ = "1-222-222-222"
+buf.push(null == __val__ ? "" : __val__);
+buf.push('</div></div></div></div></div>');
 }
 return buf.join("");
 }return { render: anonymous }; });
